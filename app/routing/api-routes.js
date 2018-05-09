7c3979557c1087af1c153d2bcd03bc5fee9b26dd
Melinda Richards <richardsmelinda@yahoo.com>
2018-05-08 19:59:31 -0600

    friendFinder

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,22 +1,22 @@
-var friendData = require("../data/friend-data.js");
+var friendArray = require("../data/friend-data.js");
 
 module.exports = function (app) {
 
 	app.get("/api/friends", function (req, res) {
-		res.json(friendData);
+		res.json(friendArray);
 	});
 
 	app.post("/api/friends", function (req, res) {
-		console.log(req.body);
+		// console.log(req.body);
 		var newFriend = {};
 		newFriend.friendName = req.body.friendName;
 		newFriend.friendPhoto = req.body.friendPhoto;
 		newFriend.friendScores = req.body.friendScores;
-
+		friendArray.push(newFriend);
 		var totalScores = [];
 
 	// This outer for loop goes through each friend in my friendArray variable	
-		for (let i = 0; i < friendArray.length; i++) {
+		for (let i = 0; i < friendArray.length-1; i++) {
 			var friendForLoop = friendArray[i];
 
 			var totalFriendScore = 0;
@@ -24,15 +24,22 @@ module.exports = function (app) {
 			// As the outer for loop goes through each individual friend in friendArray, this inner for loop goes through each number in the friendScores array
 			for (let j = 0; j < friendForLoop.friendScores.length; j++) {
 				var scoreForLoop = friendForLoop.friendScores[j];
+				// console.log(scoreForLoop);
 
 		// This variable calculates the absolute value of the difference between each individual score when comparing the new friend's score on a given question against the score for that same question of the friends already in friendArray
 		var absValue = Math.abs(newFriend.friendScores[j] - friendForLoop.friendScores[j]);
+		// This adds the absolute value of each question to get a total score for each friend (in other words, it adds up the absolute value between Sue's and Bob's answers for q1, q2, q3, etc., then does the same for Sue and John, Sue and Frank, etc.)
+			
+			// console.log(absValue);
 
-		// This adds the absolute value of each question to get a total score for each friend (in other words, it adds up the abs value between Sue's and Bob's answers for q1, q2, q3, etc., then does the same for Sue and John, Sue and Frank, etc.)
+			// Iterate over your first array and then grab the second element add the values up
+	
 		totalFriendScore +=absValue;
-		}
+		
+			console.log(totalFriendScore);
+	}
 
-		// This pushes the final totalFriendScore (after adding in the abs values for each question) into the array of scores in the variable totalScores.
+		// This pushes the final totalFriendScore (after adding in the absolute values for each question) into the array of scores in the variable totalScores.
 		totalScores.push(totalFriendScore);
 	}
 
@@ -48,17 +55,9 @@ module.exports = function (app) {
     }
 	});
 
-
 // var closestMatch =
 
 // res.json(closestMatch);
 
-	// delete
-	// app.post("/api/clear", function() {
-	// 	friendData = [];
-
-	// 	console.log(friendData);
-
-	// })
 
 	}
